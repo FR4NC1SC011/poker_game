e05c077400d401c72f829fbb164dedf6c311bde7
FR4NC1SC011 <fcootzg@gmail.com>
2022-01-11 16:44:18 -0700

    The player can select up to 3 cards and change them

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 use rand::random;
+use std::io::{self, BufRead};
 
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
@@ -11,6 +12,13 @@ struct Card {
     suit: Suits,
 }
 
+//NOTE: This structure is necessary?
+#[derive(Debug, Clone)]
+struct Deck {
+    hand: Vec<Card>
+}
+
+
 #[derive(EnumIter, Debug, Clone, Copy)]
 enum Suits {
     Clubs,
@@ -20,7 +28,7 @@ enum Suits {
 }
 
 
-#[derive(EnumIter, Debug, Clone, Copy)]
+#[derive(EnumIter, Debug, Clone, Copy, PartialEq, Eq)]
 enum Values {
     Ace,
     Two,
@@ -38,24 +46,71 @@ enum Values {
 }
 
 fn main() {
-    let mut deck: Vec<Card> = Vec::new();
-    let mut hand: Vec<Card> = Vec::new();
+    //let mut deck: Vec<Card> = Vec::new();
+    //let mut hand: Vec<Card> = Vec::new();
 
+    /*
     for s in Suits::iter() {
         for v in Values::iter() {
             let card = Card::new(v, s);
             deck.push(card);
         }
     }
+    */
+
+    let mut deck: Deck = Deck::new();
+    let mut hand: Deck = Deck::new_hand(&mut deck);
 
 
+    /*
     for _ in 0..5 {
-        let index = (random::<f32>() * deck.len() as f32).floor() as usize;
-        hand.push(deck[index]);
-        deck.remove(index);
+        let index = (random::<f32>() * deck.hand.len() as f32).floor() as usize;
+        hand.push(deck.hand[index]);
+        deck.hand.remove(index);
+    }
+    */
+
+    for x in &hand.hand {
+        x.print_card();
     }
 
-    for x in hand {
+    //NOTE: Upgrade this piece of code
+    /*
+    println!("Cards to change: ");
+    let reader = io::stdin();
+    let mut cards_to_change: Vec<i32> =
+        reader.lock()
+              .lines().next().unwrap().unwrap()
+              .split(' ').map(|s| s.trim())
+              .filter(|s| !s.is_empty())
+              .map(|s| s.parse().unwrap())
+              .collect();
+
+    cards_to_change.truncate(2);
+
+
+    let mut z = 0;
+    for x in &cards_to_change {
+
+        if x > &4 {
+            unimplemented!();
+        }
+
+        let i = x - z;
+        hand.hand.remove(i.try_into().unwrap());
+        z += 1;
+    }
+
+    for _ in 0..cards_to_change.len() {
+        let index = (random::<f32>() * deck.hand.len() as f32).floor() as usize;
+        hand.hand.push(deck.hand[index]);
+        deck.hand.remove(index);
+    }
+    */
+
+    hand = Deck::change_cards(hand, &mut deck);
+
+    for x in &hand.hand {
         x.print_card();
     }
 
@@ -77,6 +132,72 @@ impl Card {
     }
 
 }
+
+impl Deck {
+    fn new() -> Self {
+        let hand: Vec<Card> = Vec::new();
+        let mut deck: Deck = Deck { hand };
+
+        for s in Suits::iter() {
+            for v in Values::iter() {
+                let card = Card::new(v, s);
+                deck.hand.push(card);
+            }
+        }
+
+        deck
+    }
+
+    fn new_hand(deck: &mut Deck) -> Self {
+        let hand: Vec<Card> = Vec::new();
+        let mut player_hand: Deck = Deck { hand };
+        for _ in 0..5 {
+            let index = (random::<f32>() * deck.hand.len() as f32).floor() as usize;
+            player_hand.hand.push(deck.hand[index]);
+            deck.hand.remove(index);
+        }
+
+
+        player_hand
+    }
+
+    fn change_cards(mut hand: Deck, deck: &mut Deck) -> Self {
+        println!("Cards to change: ");
+        let reader = io::stdin();
+        let mut cards_to_change: Vec<i32> =
+            reader.lock()
+                .lines().next().unwrap().unwrap()
+                .split(' ').map(|s| s.trim())
+                .filter(|s| !s.is_empty())
+                .map(|s| s.parse().unwrap())
+                .collect();
+
+        cards_to_change.truncate(2);            // Mantains the len of the vector in 3   ---  0,1,2
+
+
+        let mut z = 0;
+        for x in &cards_to_change {
+
+            if x > &4 {
+                unimplemented!();
+            }
+
+            let i = x - z;
+            hand.hand.remove(i.try_into().unwrap());
+            z += 1;
+        }
+
+        for _ in 0..cards_to_change.len() {
+            let index = (random::<f32>() * deck.hand.len() as f32).floor() as usize;
+            hand.hand.push(deck.hand[index]);
+            deck.hand.remove(index);
+        }
+
+
+        hand
+    }
+}
+
 
 
 
